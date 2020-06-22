1f4f992c7e0861cd2e32b4ba0d85eb2320f41e9a
JohnOx <46574731+JohnOx@users.noreply.github.com>
2020-06-22 13:16:40 +0200

    added travel agency workspace angular

---
+++ TravelAgency/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { HeroComponent } from './hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    HeroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
